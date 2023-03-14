6306e869dba47ee93b30fdb0b89c39407fb4e762
Marek Nagy <62247071@cuni.cz>
2023-03-14 16:38:06 +0100

    [WIP] Added cache and using dnsClient.Reverse

--- dotnet/Solution.cs
+++ dotnet/Solution.cs
@@ -9,10 +9,14 @@ namespace dns_netcore
 {
 	class RecursiveResolver : IRecursiveResolver
 	{
+		// variables for debugging
 		private const bool VERBOSE = false;
+		
+
 		private readonly IDNSClient dnsClient;
 		private readonly IReadOnlyList<IP4Addr> rootServers;
 		private readonly ConcurrentDictionary<string, Task<Task<IP4Addr>>> currentlyResolving = new();
+		private readonly ConcurrentDictionary<string, IP4Addr> cache = new();
 		private readonly ConcurrentDictionary<IP4Addr, int> rootServersLoad = new();
 
 		public RecursiveResolver(IDNSClient client)
@@ -21,7 +25,7 @@ namespace dns_netcore
 			this.rootServers = client.GetRootServers();
 			foreach (var rootServer in this.rootServers)
 			{
-				rootServersLoad.TryAdd(rootServer, 0);
+				rootServersLoad[rootServer] = 0;
 			}
 		}
 
@@ -29,6 +33,7 @@ namespace dns_netcore
 			(var rootServerIp, var server_load) = rootServersLoad.Min();
 			rootServersLoad.TryUpdate(rootServerIp, server_load+1, server_load);
 			var nextServer = await dnsClient.Resolve(rootServerIp, firstSubdomain);
+			this.cache[firstSubdomain] = nextServer;
 			int later_server_load;
 			while ( ! rootServersLoad.TryGetValue(rootServerIp, out later_server_load) ) { }
 			rootServersLoad.TryUpdate(rootServerIp, later_server_load+1, later_server_load);
@@ -42,26 +47,52 @@ namespace dns_netcore
 			 * Replace it with your implementation.
 			 * Also you may change this method to async (it will work with the interface).
 			 */
-
+			
 			var subdomains = domain.Split('.').ToArray();
 			var subdomainsLength = subdomains.Length;
 			var wholeSubdomains = Enumerable.Range(1, subdomains.Length)
+				//.AsParallel()
 				.Select( index => subdomains[(subdomainsLength-index)..] )
 				.Select( subdomainsParts => String.Join('.', subdomainsParts) )
 				.ToArray();
 			Array.Reverse(subdomains);
 
 			// TODO: check if already computed IP is valid
-			int i = -1;
-			IP4Addr res;
-			Task<Task<IP4Addr>> t;
+			int i = -1;				
+			Task<Task<IP4Addr>> t = null;
 
 			for (int j = wholeSubdomains.Length - 1; j >= 0; j--)
 			{
-				if ( this.currentlyResolving.TryGetValue(wholeSubdomains[j], out var val) ){
+				var wholeSubdomain = wholeSubdomains[j];
+
+				if (VERBOSE)
+					System.Console.WriteLine($"Checking for {wholeSubdomain} in cache or currently computing for {domain}");
+
+				if ( this.cache.TryGetValue(wholeSubdomain, out var addr) ) {
+					try{
+						var subdomain = await this.dnsClient.Reverse(addr);
+						if ( subdomain == wholeSubdomain ){
+							i = j;
+							t = Task.FromResult(Task.FromResult(addr));
+						}
+						else{
+							this.cache.TryRemove(wholeSubdomain, out var _);
+							continue;
+						}
+					}
+					catch (DNSClientException) {
+						continue;
+					}
+					if (VERBOSE)
+						System.Console.WriteLine($"Found cached task for {wholeSubdomain} on {domain}");
+					break;
+				}
+
+				if ( this.currentlyResolving.ContainsKey(wholeSubdomain) ){
+					var val = this.currentlyResolving[wholeSubdomain];
 					i = j;
-					t = val;
-					//res = await await val;
+					if (VERBOSE)
+						System.Console.WriteLine($"Found currently computing task for {wholeSubdomain} on {domain}");
 					break;
 				}
 					
@@ -69,73 +100,42 @@ namespace dns_netcore
 
 			i = i == -1 ? 0 : i;
 
-			//IP4Addr res;
-			if (i == 0) {
-				// TODO: balance root servers' load
-				t = Task.FromResult(AskRootServer(subdomains[0]));
-				//var res = dnsClient.GetRootServers()[0];
-				this.currentlyResolving.TryAdd(wholeSubdomains[0], t);
-				//res = await await this.currentlyResolving[wholeSubdomains[0]];
-			}
-			else {
-				t = currentlyResolving[wholeSubdomains[i]];
+			if (VERBOSE) {
+				System.Console.WriteLine($"Continuing DNS from {i} -> {wholeSubdomains[i]}");
 			}
 
-			// t = Task.Run(async () => {
-			// 	var value = await AskRootServer(subdomains[0]);
-			// 	return Task.FromResult(value);
-			// });
+			//IP4Addr res;
+			if (i == 0 && t == null) {
+				// TODO: balance root servers' load
+				t = Task.FromResult( AskRootServer(subdomains[0]) );
+				this.currentlyResolving[wholeSubdomains[0]] = t;
+			}
+			
+			t = t ?? currentlyResolving[wholeSubdomains[i]];
 
 			if (VERBOSE)
 				System.Console.WriteLine($"Started first task on {domain}");
 			
-			//this.currentlyResolving.TryAdd(wholeSubdomains[i], t);
-
-			
-			foreach (var (subdomain, wholeSubdomain) in Enumerable.Zip(subdomains.Skip(i+1), wholeSubdomains.Skip(i+1)) )
+			foreach (var (subdomain, wholeSubdomain) in Enumerable.Zip(subdomains[(i+1)..], wholeSubdomains[(i+1)..]) )
 			{
 				if (VERBOSE)
 					System.Console.WriteLine($"Creating task for {subdomain} {wholeSubdomain} for {domain}");
 				t = t.ContinueWith(async addr => {
-					var addrTaskRes = await addr.Result;
-					if (VERBOSE)
-						System.Console.WriteLine($"Starting subtask on {subdomain} \"{wholeSubdomain}\", part of {domain} on server {addrTaskRes}");
-					var value = dnsClient.Resolve(addrTaskRes, subdomain);
-					if (VERBOSE)
-						System.Console.WriteLine($"Resolved {subdomain} on {domain}");
-					this.currentlyResolving.TryAdd(wholeSubdomain, Task.FromResult(value) );
-					var valueRes = this.currentlyResolving[wholeSubdomain].Result;
-					if (VERBOSE)
-						System.Console.WriteLine($"Ending subtask {subdomain} on {wholeSubdomain}: server {valueRes}");
-					return await valueRes;
-				}, TaskContinuationOptions.PreferFairness);
+					var addrTaskRes = await addr;
+					return await await addrTaskRes.ContinueWith(async serverAddr => {
+						var serverIP = await serverAddr;
+						if (VERBOSE)
+							System.Console.WriteLine($"Starting subtask on {subdomain} \"{wholeSubdomain}\", part of {domain} on server {serverIP}");
+						var value = await dnsClient.Resolve(serverIP, subdomain);
+						this.cache[wholeSubdomain] = value;
+						this.currentlyResolving.TryRemove(wholeSubdomain, out var _);
+						return value;
+					});
+				});
+				this.currentlyResolving[wholeSubdomain] = t;
 			}
 
 			var finalAddr = await await t;
-
-			// for (i = ++i; i < subdomainsLength; i++)
-			// {
-			// 	var subdomain = subdomains[i];
-			// 	var wholeSubdomain = allSubdomains[i];
-			// 	// ? TODO: check if subdomain is being computed
-			// 	if ( this.currentlyResolving.TryGetValue(wholeSubdomain, out var val) ) {
-			// 		res = await val;
-			// 	}
-			// 	else {
-			// 		this.currentlyResolving[wholeSubdomain] = dnsClient.Resolve(res, subdomain);
-			// 		res = await this.currentlyResolving[wholeSubdomain];
-			// 	}
-			// }
-
-			// // ? TODO: remove addresses for all subdomains
-			// // what if we remove subpart of second resolving?
-			foreach (var subdomain in wholeSubdomains)
-			{
-				if ( this.currentlyResolving.TryRemove(subdomain, out var val) ) {
-
-				}
-			}
-
 			return finalAddr;
 		}
 	}
